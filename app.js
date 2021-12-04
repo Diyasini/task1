4f56f44cb1fcf5dd53941d991ff94896b54faefb
Diyasini <90218870+Diyasini@users.noreply.github.com>
2021-12-04 18:58:19 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const loginText = document.querySelector(".title-text .login");
+      const loginForm = document.querySelector("form.login");
+      const loginBtn = document.querySelector("label.login");
+      const signupBtn = document.querySelector("label.signup");
+      const signupLink = document.querySelector("form .signUp-link a");
+      const LoginLink = document.querySelector("form .login-link a");
+      signupBtn.onclick = (()=>{
+        loginForm.style.marginLeft = "-50%";
+        loginText.style.marginLeft = "-50%";
+      });
+      loginBtn.onclick = (()=>{
+        loginForm.style.marginLeft = "0%";
+        loginText.style.marginLeft = "0%";
+      });
+      signupLink.onclick = (()=>{
+        signupBtn.click();
+        return false;
+      });
+      LoginLink.onclick = (()=>{
+        loginBtn.click();
+        return false;
+      });
